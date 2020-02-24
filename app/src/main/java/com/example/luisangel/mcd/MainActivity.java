b5d1448bbb34a9c5759d31a029cd579c0fbd5e3c
Vero <veronica.rg.pb@gmail.com>
2020-02-24 07:31:03 -0600

    Actualizacion 0.5

--- app/src/main/java/com/example/luisangel/mcd/MainActivity.java
+++ app/src/main/java/com/example/luisangel/mcd/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.luisangel.mcd;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView resultado;
     EditText num1,num2;
     Button boton,boton2,boton3;
-    int b,c;
+    int b,c,pulasiones;
     Funciones func;
+    Log log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,19 @@ public class MainActivity extends AppCompatActivity {
                 b = Integer.parseInt(p);
                 c = Integer.parseInt(q);
                 resultado.setText(""+func.mcm(b,c));
+            }
+        });
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                String prox = "Diganle a erik";
+                String eric = "Diganle a Erik que traiga cacachuates para desbloquear esta opcion";
+                int duration = Toast.LENGTH_SHORT;
+                log.d("Aqui","aqui");
+                Toast toast = Toast.makeText(context,eric,duration);
+                toast.show();
             }
         });
     }
